e4ac889651a787187bcc392a8fffb19ec7927b8a
JeremyStee <jeremy.steenkiste.pro@gmail.com>
2018-09-24 14:45:25 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
+
+//Components
+import Home from './js/Home';
+import NotFound from './js/NotFound';
+import Fiche from './js/Fiche';
+
+//SCSS
 import './scss/App.scss';
 
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Salut</h1>
+                <Router history={hashHistory}>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/fiche" component={Fiche} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </Router>
             </div>
         );
     }
---
+++ src/js/Fiche.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Fiche extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Fiche perso</h1>
+            </div>
+        );
+    }
+}
+
+export default Fiche;
